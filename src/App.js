71d9f1c6e70503f5d0c8c42de0d771b2a9930630
ANjitGC741 <anzit741@gmail.com>
2023-04-29 19:56:11 +0545

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import programming from "../src/img/programming.png";
+import santaClaus from "../src/img/santaClaus.png";
+import scream from "../src/img/scream.png";
+import shuffle from "../src/img/shuffle.png";
+import voodooDoll from "../src/img/voodooDoll.png";
+import img1 from "../src/img/img1.png";
+import img2 from "../src/img/img2.png";
+import "./App.css";
 function App() {
+  const [type, setType] = useState(["Any"]);
+  const [jokes, getJokes] = useState([]);
+  const fetchJokes = () => {
+    fetch(`https://v2.jokeapi.dev/joke/${type ? type : "Any"}`)
+      .then((response) => response.json())
+      .then((data) => getJokes(data));
+  };
+  useEffect(() => {
+    fetchJokes();
+  }, [type]);
+  const handleOptionChange = (event) => {
+    setType(event.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-div">
+      <div className="category-section">
+        <h3>Select Category</h3>
+        <form>
+          <input
+            type="radio"
+            name="jokesType"
+            value="Programming"
+            onChange={handleOptionChange}
+          />
+          <label>
+            <img src={programming} className="img-icon" />
+          </label>
+          <input
+            type="radio"
+            name="jokesType"
+            value="Misc"
+            onChange={handleOptionChange}
+          />
+          <label>
+            <img src={shuffle} className="img-icon" />
+          </label>
+          <input
+            type="radio"
+            name="jokesType"
+            value="Dark"
+            onChange={handleOptionChange}
+          />
+          <label>
+            <img src={voodooDoll} className="img-icon" />
+          </label>
+          <input
+            type="radio"
+            name="jokesType"
+            value="Spooky"
+            onChange={handleOptionChange}
+          />
+          <label>
+            <img src={scream} className="img-icon" />
+          </label>
+          <input
+            type="radio"
+            name="jokesType"
+            value="Christmas"
+            onChange={handleOptionChange}
+          />
+          <label>
+            <img src={santaClaus} className="img-icon" />
+          </label>
+        </form>
+      </div>
+      <div className="image-box-one">
+          <img src={img1} className="meme-img"/>
+          <p className="meme-text-box text-box-one">{jokes?.setup}</p>
+      </div>
+      <div className="image-box-two">
+      <img src={img2} className="meme-img"/>
+      <p className="meme-text-box text-box-two">{jokes?.delivery ? jokes?.delivery : ""}</p>
+      </div>
+      {/* <p>{jokes?.setup}</p> */}
+      {/* <p>{jokes?.delivery ? jokes?.delivery : ""}</p> */}
+      <button className="another-one-btn" onClick={() => fetchJokes()}>Another one</button>
     </div>
   );
 }
